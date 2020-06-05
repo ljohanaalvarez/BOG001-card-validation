c4a0f78e809595bfa6275dd40057e377f6aed4ee
lizguro <dianyguti@gmail.com>
2020-06-05 13:23:25 -0500

    capturar numero de tarjeta y crear array

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-import validator from './validator.js';
+//import validator from './validator.js';
 
-console.log(validator);
+//console.log(validator);
+
+let numberCard = document.getElementById("card");
+let btnValidate = document.getElementById ("vCard");
+
+btnValidate.onclick = function(){
+
+    let nCard= numberCard.value;
+
+    let num = Array.from(nCard);
+    let revers = num.reverse();
+    console.log(nCard);
+    console.log(num);
+    
+    for (let i=0; i <= num.length(); i++) {
+
+        const entero[i]= parseInt(num(i));
+        let arrayNCard = arrayNCard.push(entero);
+        console.log(arrayNCard);
+        break
+  
+    }
+
+    
+    
+}
