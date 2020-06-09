c831df07180aa8b1a4e4f6fb057fe3d30349672e
lizguro <dianyguti@gmail.com>
2020-06-09 11:55:33 -0500

    javascript hasta creacion array enteros

--- src/index.js
+++ src/index.js
@@ -1,28 +1,50 @@
-//import validator from './validator.js';
+import validator from './validator.js';
 
-//console.log(validator);
+console.log(validator);
 
 let numberCard = document.getElementById("card");
 let btnValidate = document.getElementById ("vCard");
+
 
 btnValidate.onclick = function(){
 
     let nCard= numberCard.value;
 
     let num = Array.from(nCard);
-    let revers = num.reverse();
-    console.log(nCard);
+    num.reverse();
+   
     console.log(num);
     
-    for (let i=0; i <= num.length(); i++) {
-
-        const entero[i]= parseInt(num(i));
-        let arrayNCard = arrayNCard.push(entero);
-        console.log(arrayNCard);
-        break
-  
-    }
-
+    /* otra forma de hacer entero un numero dentro de de un array
+    let multipleNumbers = num;
+    let multipleNumbers2 = multipleNumbers.map(multiple);
     
+    console.log(multipleNumbers2);
+
+    function multiple (value, index, array){
+        for (let index=0; index > array.lenght; index ++){
+            return parseInt(value);
+        }
+        
+    } */
+    let arrayNCard = [];
+    for (let i=0; i < num.length; i++) {
+
+        const entero= parseInt(num[i]);
+        arrayNCard.push(entero*2);
+        
+        if(espar(i)){
+            
+        }
+
+       }
     
+       console.log(arrayNCard);
+
 }
+
+function espar(i){
+    if (i);
+    return true;
+}
+
