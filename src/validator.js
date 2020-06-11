1f38100e3592fb45aeb53163055dff8701a9c30e
ljohanalvarez <l.johanaalvarez@gmail.com>
2020-06-11 16:55:27 -0500

    javascript js

--- src/validator.js
+++ src/validator.js
@@ -1,13 +1,6 @@
 const validator = {
-<<<<<<< HEAD
-    
-  };
-  
-  export default validator;
-=======
   // ...np
 };
 
 export default validator;
 
->>>>>>> 2b7fcc70c447e751898e727c4743068d8c7f4a97
